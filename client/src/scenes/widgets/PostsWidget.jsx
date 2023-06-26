7f72f5aa322a1b9c3a976780346b13556fd02b28
Nilavo <iem.nilavo2001@gmail.com>
2023-06-27 03:40:56 +0530

    finishing the inital version

---
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -0,0 +1,62 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
+import PostWidget from "./PostWidget";
+
+const PostsWidget = ({ userId, isProfile = false }) => {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts);
+  const token = useSelector((state) => state.token);
+  console.log("post-widget called");
+  const getPosts = async () => {
+    console.log("fetching post from posts widget");
+    const response = await fetch("http://localhost:3001/posts", {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    dispatch(setPosts({ posts: data }));
+  };
+
+  const getUserPosts = async () => {
+    console.log("fetching post from posts widget");
+    const response = await fetch(
+      `http://localhost:3001/posts/${userId}/posts`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    dispatch(setPosts({ posts: data }));
+  };
+
+  useEffect(() => {
+    if (isProfile) {
+      getUserPosts();
+    } else {
+      getPosts();
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <>
+      {posts.map(({ _id, user, description, picturePath, likes, comments }) => (
+        <PostWidget
+          key={_id}
+          postId={_id}
+          postUserId={user._id}
+          name={`${user.firstName} ${user.lastName}`}
+          description={description}
+          location={user.location}
+          picturePath={picturePath}
+          userPicturePath={user.picturePath}
+          likes={likes}
+          comments={comments}
+        />
+      ))}
+    </>
+  );
+};
+
+export default PostsWidget;
